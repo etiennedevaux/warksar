a3ddd4cdf1f44c70b2450b34a6044fb1ac7e9b04
Stephen Clarke <60775095+etiennedevaux@users.noreply.github.com>
2021-01-08 16:22:33 +0000

    First draft to establish look and feel

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# warksar
+# Website for Warwickshire Search and Rescue
+
+# Stored in the Github directory warksar
+
+## Currently not approved for any form of public release
---
+++ assets/js/warwickshiresearchandrescue.js
@@ -0,0 +1,14 @@
+// Global Variables
+var jsfileversion="0003";
+
+function showMenuComponent(sect) {
+var elements = document.getElementsByClassName('MenuComponent');
+
+    for (var i = 0; i < elements.length; i++){
+        elements[i].style.display = 'none';
+    }
+var x = document.getElementById(sect);
+x.style.display = 'block';
+document.getElementById('JSSerNo').textContent='.'+jsfileversion;
+}
+
